c408e709fb333281dd314350f71e813a3e8ea065
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-06-14 17:17:52 +0500

    Updated

---
+++ lib/calculator.widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+
+  _Calculator createState() => _Calculator();
+}
+
+class _Calculator extends State {
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      child: Center(
+        child:Text('Welcome')
+      )
+    );
+  }
+}
