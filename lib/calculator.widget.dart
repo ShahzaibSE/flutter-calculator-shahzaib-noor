0802eec41edec5ec406afe52976da0158076c0bb
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-06-14 19:52:14 +0500

    Updated

--- lib/calculator.widget.dart
+++ lib/calculator.widget.dart
@@ -9,64 +9,66 @@ class _Calculator extends State {
   @override
   Widget build(BuildContext context){
     return Container(
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              width: 50,
-              height: 50,
-              child: ElevatedButton(
-                style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0)
-                  )
-                )),
-                child: Text('1'),
-                onPressed: (){},
-              )
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              child: ElevatedButton(
-                style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0)
-                  )
-                )),
-                child: Text('2'),
-                onPressed: (){},
-              )
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              child: ElevatedButton(
-                style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0)
-                  )
-                )),
-                child: Text('3'),
-                onPressed: (){},
-              )
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              child: ElevatedButton(
-                style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(50.0)
-                  )
-                )),
-                child: Text('/'),
-                onPressed: (){},
-              )
-            ),
-          ],
-        )
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                child: ElevatedButton(
+                  style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)
+                    )
+                  )),
+                  child: Text('1'),
+                  onPressed: (){},
+                )
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                child: ElevatedButton(
+                  style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)
+                    )
+                  )),
+                  child: Text('2'),
+                  onPressed: (){},
+                )
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                child: ElevatedButton(
+                  style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)
+                    )
+                  )),
+                  child: Text('3'),
+                  onPressed: (){},
+                )
+              ),
+              Container(
+                width: 80,
+                height: 80,
+                child: ElevatedButton(
+                  style: ButtonStyle(shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50.0)
+                    )
+                  )),
+                  child: Text('/'),
+                  onPressed: (){},
+                )
+              ),
+            ],
+          )
+        ]
       )
     );
   }
